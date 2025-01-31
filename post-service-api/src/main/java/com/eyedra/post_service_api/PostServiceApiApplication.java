e35dda57ac80a95677efa21170aa7f201c132643
Yohan Joshua Sandarasekara <74724437+Joshu03@users.noreply.github.com>
2025-01-31 22:06:10 +0530

    Post service api Created

---
+++ post-service-api/src/main/java/com/eyedra/post_service_api/PostServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.eyedra.post_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostServiceApiApplication.class, args);
+	}
+
+}
---
+++ post-service-api/src/test/java/com/eyedra/post_service_api/PostServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyedra.post_service_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
