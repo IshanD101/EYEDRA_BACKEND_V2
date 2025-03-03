deb42b7f81f665cd9d839df6e010b07509f1acd3
Yohan Joshua Sandarasekara <74724437+Joshu03@users.noreply.github.com>
2025-03-03 14:39:47 +0530

    mongoDB connected
    MongoDB connected Successfully

--- comment-service-api/src/main/java/com/eyedra/comment_service_api/config/SecurityConfig.java
+++ comment-service-api/src/main/java/com/eyedra/comment_service_api/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.eyedra.comment_service_api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 
 
 @Configuration
@@ -16,7 +18,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-            .antMatchers("/comments/**").authenticated()
+            .antMatchers("/comments/**").permitAll()
+
+
+
             .anyRequest().permitAll()
             .and()
             .addFilterBefore(new JwtAuthenticationFilter(authenticationManager()), UsernamePasswordAuthenticationFilter.class);
