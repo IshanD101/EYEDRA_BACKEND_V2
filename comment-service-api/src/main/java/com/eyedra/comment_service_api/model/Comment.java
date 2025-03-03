82b174ad843e9c4eebd8bafc0f8792ef1bb1e2ac
Yohan Joshua Sandarasekara <74724437+Joshu03@users.noreply.github.com>
2025-03-03 10:13:47 +0530

    Create Comment.java
    Created the Model

---
+++ comment-service-api/src/main/java/com/eyedra/comment_service_api/model/Comment.java
@@ -0,0 +1,46 @@
+package com.eyedra.comment_service_api.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "comments")
+public class Comment {
+    @Id
+    private String id;
+    private String text;
+    private String userId;
+    private int reactions; // For heart reactions
+
+    // Getters and Setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public int getReactions() {
+        return reactions;
+    }
+
+    public void setReactions(int reactions) {
+        this.reactions = reactions;
+    }
+}
