aafd240900a1822481e7ac28eb74c4fdea87d6c2
IshanD101 <dakshinaishan88@gmail.com>
2025-03-06 11:02:05 +0530

    User controller and dto files changes done

--- user-service-api/src/main/java/com/eyedra/user_service_api/dto/request/RegisterReqDto.java
+++ user-service-api/src/main/java/com/eyedra/user_service_api/dto/request/RegisterReqDto.java
@@ -5,7 +5,7 @@ import lombok.*;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AuthReqDto {
+public class RegisterReqDto {
     private String username;
     private String password;
     private String email;
