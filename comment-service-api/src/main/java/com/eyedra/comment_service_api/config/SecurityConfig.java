656f383b95e6212adf6305dfa0c5da7b60485d74
Yohan Joshua Sandarasekara <74724437+Joshu03@users.noreply.github.com>
2025-03-08 20:08:25 +0530

    updated
    fixing bugs

--- comment-service-api/src/main/java/com/eyedra/comment_service_api/config/SecurityConfig.java
+++ comment-service-api/src/main/java/com/eyedra/comment_service_api/config/SecurityConfig.java
@@ -1,29 +1,21 @@
 package com.eyedra.comment_service_api.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-            .antMatchers("/comments/**").permitAll()
-
-
-
+            .requestMatchers("/comments/**").permitAll()
             .anyRequest().permitAll()
-            .and()
-            .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
-
+            .and();
+        return http.build();
     }
 }
