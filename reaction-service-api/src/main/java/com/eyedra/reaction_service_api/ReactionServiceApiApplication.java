e5bd0a82c918f992d734bba7e107f38350221db6
Yohan Joshua Sandarasekara <74724437+Joshu03@users.noreply.github.com>
2025-03-09 03:10:26 +0530

    initial Reaction Service API
    Created New Reaction Microservices (SpringBoot)

---
+++ reaction-service-api/src/main/java/com/eyedra/reaction_service_api/ReactionServiceApiApplication.java
@@ -0,0 +1,13 @@
+package com.eyedra.reaction_service_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactionServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactionServiceApiApplication.class, args);
+	}
+
+}
