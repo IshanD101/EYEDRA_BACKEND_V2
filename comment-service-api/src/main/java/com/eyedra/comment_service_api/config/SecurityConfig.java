98c5ad1c1e5bd9bda816dd7022826087e80489dc
Yohan Joshua Sandarasekara <74724437+Joshu03@users.noreply.github.com>
2025-03-08 20:09:15 +0530

    Update SecurityConfig.java

--- comment-service-api/src/main/java/com/eyedra/comment_service_api/config/SecurityConfig.java
+++ comment-service-api/src/main/java/com/eyedra/comment_service_api/config/SecurityConfig.java
@@ -1,21 +1,22 @@
 package com.eyedra.comment_service_api.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-            .requestMatchers("/comments/**").permitAll()
+            .antMatchers("/comments/**").permitAll()
             .anyRequest().permitAll()
             .and();
-        return http.build();
     }
 }
