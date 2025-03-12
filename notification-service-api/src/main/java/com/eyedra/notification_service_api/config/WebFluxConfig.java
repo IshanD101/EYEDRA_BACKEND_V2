2496495ae76b07ac082082bf7dc7103085f01017
Yohan Joshua Sandarasekara <74724437+Joshu03@users.noreply.github.com>
2025-03-13 00:01:28 +0530

    Create WebFluxConfig.java

---
+++ notification-service-api/src/main/java/com/eyedra/notification_service_api/config/WebFluxConfig.java
@@ -0,0 +1,29 @@
+package com.eyedra.notification_service_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.CorsRegistry;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebFluxConfig implements WebFluxConfigurer {
+
+    @Bean
+    public WebClient.Builder webClientBuilder() {
+        return WebClient.builder();
+    }
+
+    @Bean
+    public WebClient webClient(WebClient.Builder webClientBuilder) {
+        return webClientBuilder.build();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
