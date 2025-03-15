46fc9ee78dc175fc044fbfbb1121a568b4db17f0
IshanD101 <dakshinaishan88@gmail.com>
2025-03-15 15:39:01 +0530

    web client config recreated

--- community-service-api/src/main/java/com/eyedra/community_service_api/configs/WebClientConfig.java
+++ community-service-api/src/main/java/com/eyedra/community_service_api/configs/WebClientConfig.java
@@ -2,13 +2,18 @@ package com.eyedra.community_service_api.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
 
     @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
+    public WebClient userServiceWebClient() {
+        return WebClient.builder()
+                .baseUrl("http://user-service-api/api/users")
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build();
     }
 }
