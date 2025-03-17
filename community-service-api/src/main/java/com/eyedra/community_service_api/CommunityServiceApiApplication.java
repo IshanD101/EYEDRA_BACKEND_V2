a95a2f76924453841ff4bcc477c65e97a272a046
IshanD101 <dakshinaishan88@gmail.com>
2025-03-17 14:42:24 +0530

    Discovery client done

--- community-service-api/src/main/java/com/eyedra/community_service_api/CommunityServiceApiApplication.java
+++ community-service-api/src/main/java/com/eyedra/community_service_api/CommunityServiceApiApplication.java
@@ -2,9 +2,10 @@ package com.eyedra.community_service_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class CommunityServiceApiApplication {
 
 	public static void main(String[] args) {
