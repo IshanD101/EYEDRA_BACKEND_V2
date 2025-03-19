92c3ba653c648b7ba948e7c211035edb53328472
IshanD101 <dakshinaishan88@gmail.com>
2025-03-19 14:17:25 +0530

    Eureka client enabled

--- gateway-service-api/src/main/java/com/eyedra/gateway_service_api/GatewayServiceApiApplication.java
+++ gateway-service-api/src/main/java/com/eyedra/gateway_service_api/GatewayServiceApiApplication.java
@@ -2,9 +2,10 @@ package com.eyedra.gateway_service_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-
+@EnableDiscoveryClient
 public class GatewayServiceApiApplication {
 
 	public static void main(String[] args) {
